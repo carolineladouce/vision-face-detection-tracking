529f573de61415b284b293c944e96497c8cd5ace
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-09 18:45:07 -0500

    Deleted Main Storyboard Set up project to be built using UIKit Created instance of class AVCaptureSession Added func addCameraInput()

--- VisionFaceDetectionTracking/MainViewController.swift
+++ VisionFaceDetectionTracking/MainViewController.swift
@@ -6,8 +6,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MainViewController: UIViewController {
+    
+    private let captureSession = AVCaptureSession()
+    
+    private func addCaeraInput() {
+        guard let device = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera, .builtInDualCamera, .builtInTrueDepthCamera], mediaType: .video, position: .front).devices.first else {
+            fatalError("No back camera device found")
+        }
+        
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
