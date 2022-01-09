a08bf58091ee91fa7211e974e633410843a9aa0f
Caroline LaDouce <39711539+carolineladouce@users.noreply.github.com>
2022-01-09 18:33:47 -0500

    Initial Commit

---
+++ VisionFaceDetectionTracking/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  VisionFaceDetectionTracking
+//
+//  Created by Caroline LaDouce on 1/9/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
---
+++ VisionFaceDetectionTrackingUITests/VisionFaceDetectionTrackingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VisionFaceDetectionTrackingUITestsLaunchTests.swift
+//  VisionFaceDetectionTrackingUITests
+//
+//  Created by Caroline LaDouce on 1/9/22.
+//
+
+import XCTest
+
+class VisionFaceDetectionTrackingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
